bddebaaf1ff9e01c28b1df708252cd6806a7f515
GeneralDoran <dorna35@libertycommon.org>
2025-01-25 09:48:54 -0700

    Crunch time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/voidvision/stagetwo/Sample_Base.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.voidvision.stagetwo;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class Sample_Base extends Auto_Framework{
+    @Override
+    public void runOpMode() throws InterruptedException{
+        //Define initial Position
+        beginPose = new Pose2d(0, 28, 0);
+        //Instantiate Mec Drive
+        drive = new MecanumDrive(hardwareMap, beginPose);
+        Action runSpecialTestRun = drive.actionBuilder(beginPose)
+                //.strafeTo(new Vector2d(21,28-5))
+                //.strafeTo(new Vector2d(25+.1,28-5))
+
+                .strafeTo(new Vector2d(8,70))
+                .turn((135/360d)*fullTurn)
+                .waitSeconds(.5)
+                .turn((135/360d)*fullTurn*-1)
+
+                .waitSeconds(.2)
+                .strafeTo(new Vector2d(33,57+4))
+
+                .strafeTo(new Vector2d(8,70))
+                .turn((135/360d)*fullTurn)
+                .waitSeconds(.5)
+                .turn((135/360d)*fullTurn*-1)
+
+                .waitSeconds(.2)
+                .strafeTo(new Vector2d(33,57+4+9.25))
+
+                .strafeTo(new Vector2d(8,70))
+                .turn((135/360d)*fullTurn)
+                .waitSeconds(.5)
+                .turn((135/360d)*fullTurn*-1)
+
+                .waitSeconds(.2)
+                .strafeTo(new Vector2d(33,57+4+2*(9.25)))
+
+                .strafeTo(new Vector2d(8,70))
+                .turn((135/360d)*fullTurn)
+                .waitSeconds(.5)
+                .turn((135/360d)*fullTurn*-1)
+
+                .waitSeconds(.2)
+                .strafeTo(new Vector2d(13,63))
+
+                .build();
+        Actions.runBlocking(
+                runSpecialTestRun
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/voidvision/stagetwo/Specimen_Base.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.voidvision.stagetwo;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class Specimen_Base extends Auto_Framework{
+    @Override
+    public void runOpMode() throws InterruptedException{
+        //Define initial Position
+        beginPose = new Pose2d(0, 0, 0);
+        //Instantiate Mec Drive
+        drive = new MecanumDrive(hardwareMap, beginPose);
+        Action runSpecialTestRun = drive.actionBuilder(beginPose)
+                .strafeTo(new Vector2d(25.1,6))
+                .strafeTo(new Vector2d(25+.1,-22))
+                .strafeTo(new Vector2d(49,-22))
+                .strafeTo(new Vector2d(49,-32))
+                .strafeTo(new Vector2d(9,-32))
+                .strafeTo(new Vector2d(49,-32))
+                .strafeTo(new Vector2d(49,-41))
+                .strafeTo(new Vector2d(9,-41))
+                .strafeTo(new Vector2d(15,-41))
+
+                //.strafeTo(new Vector2d(21,-20))
+                //.turn((180/360d)*fullTurn)
+                //.strafeTo(new Vector2d(4,-16))
+
+                .waitSeconds(.5)
+
+                .strafeTo(new Vector2d(15,-16))
+                .waitSeconds(.5)
+                .strafeTo(new Vector2d(26.1-.5,8))//score
+                .waitSeconds(.5)
+
+                .strafeTo(new Vector2d(15,-16))
+                .waitSeconds(.5)
+                .strafeTo(new Vector2d(26.1-.5,8))//score
+                .waitSeconds(.5)
+
+                .build();
+        Actions.runBlocking(
+                runSpecialTestRun
+        );
+    }
+}
